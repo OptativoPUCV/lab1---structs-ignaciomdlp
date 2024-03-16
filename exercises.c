9a2b74310b388afddab521560ed8ed711edec199
Your Name <you@example.com>
2024-03-16 17:12:54 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -72,7 +72,7 @@ Descripción: Escribe una función que tome dos arreglos
 ordenados y sus tamaños, y luego fusione estos dos
 arreglos en un tercer arreglo también ordenado.
 */
-void mergeSortedArrays(int arr1[], int size1, int arr2[], int size2,int result[]){
+void mergeSortedArrays(int arr1[], int size1, int arr2[], int size2,int *result[]){
   
   int result[] = (int)malloc((size1 + size2)*sizeof(int));
   int i = 0;
