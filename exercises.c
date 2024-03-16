4c1b1a5a548abca7625697974c9b5e516df8442c
Your Name <you@example.com>
2024-03-16 17:33:06 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -53,9 +53,9 @@ int *filterEvenNumbers(int arr[], int size, int *newSize){
   }
   
   
-  int arrPar[];
+  int *arrPar[];
   int *arrPar = (int *)malloc(cantPares*sizeof(int));
-  if (arrPar == NULL) exit(EXIT_FAILURE);
+  if (*arrPar == NULL) exit(EXIT_FAILURE);
   
   int aux = 0;
   for (int i = 0; i < size; i++){
